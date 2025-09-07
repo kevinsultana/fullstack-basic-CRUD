50accb4afc947d0898912cef86d1719e0f53ebcc
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-07 22:00:33 +0700

    feat: complete CRUD backend with mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import router from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -9,72 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.json({ success: true, data: products });
-  } catch (error) {
-    console.error("error, fetching products", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-  if (
-    !product.name ||
-    !product.price ||
-    !product.description ||
-    !product.image
-  ) {
-    return res
-      .status(400)
-      .json({ success: false, message: "All fields are required" });
-  }
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("error, creating product", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    if (!deletedProduct) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Product not found" });
-    }
-    res.json({ success: true, message: "Product deleted successfully" });
-  } catch (error) {
-    console.error("error, deleting product", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const updates = req.body;
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, updates, {
-      new: true,
-    });
-    if (!updatedProduct) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Product not found" });
-    }
-    res.json({ success: true, data: updatedProduct });
-  } catch (error) {
-    console.error("error, updating product", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
+app.use("/api/products", router);
 
 app.listen(5000, () => {
   connectDB();

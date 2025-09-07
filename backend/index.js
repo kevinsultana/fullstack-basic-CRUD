0ea233e007970f7bb245b376f527ce9985fe1ed5
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-07 22:08:38 +0700

    feat: initialize frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use("/api/products", router);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   connectDB();
-  console.log("Server running... http://localhost:5000");
+  console.log(`Server running... http://localhost:${process.env.PORT}`);
 });

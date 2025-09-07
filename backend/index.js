ffa1426c5548d0dc7d91c782761a3824db9d2728
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-08 00:02:26 +0700

    feat: init zustand and axios for creating products

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 import router from "./routes/product.route.js";
 
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", router);
